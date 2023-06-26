48e96f7ac4b4f42dc78c5bc4038f8e128ebba12e
embabalakrishna <46039648+embabalakrishna@users.noreply.github.com>
2023-06-26 12:01:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PIL import Image
 st.write("1962 Telangana Services")
-image = Image.open('cow.png')
-st.image(image, caption='Sunrise by the mountains')
+image = Image.open('cow.jpg')
+st.image(image, caption='Save Coves')
 x="""
 This is The 1962 Telangana give full details about 1962 regarding Call counts ,Trips ,UAC% ,Animal treated Etc. 
 """
