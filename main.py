9397a3520fee7bf34e0c0366fe4afa9d71fe8f17
embabalakrishna <46039648+embabalakrishna@users.noreply.github.com>
2023-06-26 11:44:40 +0530

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.write("1962 Telangana Services")
+x="""
+This is a 1962 Telangana Services give full details about 1962 regarding Call counts ,Trips ,UAC% ,Animal treated Etc. 
+"""
+st.write(x)
